01ae6d6f06fc9d6b55948c5c78324403d5941b90
Enakhe <izuagbesam@gmail.com>
2025-07-17 22:24:43 +0100

    Add condition to give the user 5 trials

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import random
 # computer_guess is a random number from 1 to 100
 computer_guess = random.randint(1, 100)
 
+user_life = 5
 user_guess = None
 
-while user_guess != computer_guess:
+while user_guess != computer_guess and user_life != 0:
     user_guess = input("Guess a number between 1 and 100: ")
 
     if not user_guess.isdigit():
@@ -13,9 +14,13 @@ while user_guess != computer_guess:
         continue
 
     if int(user_guess) < computer_guess:
+        user_life -= 1
         print("Too low, try again!")
+        print(f"You only have {user_life} trial left")
     elif int(user_guess) > computer_guess:
+        user_life -= 1
         print("Too high, try again!")
+        print(f"You only have {user_life} trial left")
     else:
         print("  You got it! the number I guess was", computer_guess)
         break
